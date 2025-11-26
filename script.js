5aa90673e6afedff182643e854fa5ca4d214fd31
alemandiazcarlos70-ctrl <alemandiazcarlos70@gmail.com>
2025-11-25 23:28:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ rosas.forEach(rosa => {
   rosa.addEventListener("click", () => {
     if (rosa.dataset.florecida === "false") {
       petalos.forEach(p => p.style.transform = "scale(1)");
-      tallo.style.opacity = "1"; // aparece el tallo al florecer
+      tallo.style.opacity = "1"; // aparece el tallo
       rosa.dataset.florecida = "true";
     }
 
     const todasFlorecidas = Array.from(rosas).every(r => r.dataset.florecida === "true");
     if (todasFlorecidas) {
       document.body.style.backgroundColor = "skyblue";
-      sol.style.opacity = "1"; // aparece el sol
+      sol.style.opacity = "1";
     }
   });
 });
