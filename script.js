a46470426dd8f164cec182e56edaeeb3e8380b9b
alemandiazcarlos70-ctrl <alemandiazcarlos70@gmail.com>
2025-11-25 23:31:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ const sol = document.getElementById("sol");
 
 rosas.forEach(rosa => {
   const petalos = rosa.querySelectorAll(".petalo");
-  const tallo = rosa.querySelector(".tallo");
 
   rosa.addEventListener("click", () => {
     if (rosa.dataset.florecida === "false") {
       petalos.forEach(p => p.style.transform = "scale(1)");
-      tallo.style.opacity = "1"; // aparece el tallo
       rosa.dataset.florecida = "true";
+
+      // Crear el tallo dinámicamente
+      const tallo = document.createElement("div");
+      tallo.classList.add("tallo");
+      rosa.appendChild(tallo);
     }
 
     const todasFlorecidas = Array.from(rosas).every(r => r.dataset.florecida === "true");
