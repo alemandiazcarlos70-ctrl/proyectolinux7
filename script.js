13126c887e583f51c311f9b08c9bbaeceab85f92
alemandiazcarlos70-ctrl <alemandiazcarlos70@gmail.com>
2025-11-25 21:40:03 -0600

    Fix formatting issues in script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Imagen de rosa florecida
 const rosaFlorecida = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Rose_flower.jpg/120px-Rose_flower.jpg";
-
 const rosas = document.querySelectorAll(".rosa");
 
 rosas.forEach(rosa => {
